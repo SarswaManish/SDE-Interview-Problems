33054e51ba0bd7e8c61dbde651c7bf6f1a354680
SarswaManish <48132119+SarswaManish@users.noreply.github.com>
2020-08-29 10:39:43 +0530

    Create Symmetric tree recursive.cpp

---
+++ Sheet Solutions/binary tree/Symmetric tree recursive.cpp
@@ -0,0 +1,34 @@
+
+https://leetcode.com/problems/symmetric-tree/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool ismirror(TreeNode *r1,TreeNode *r2)
+    {
+        if(r1==NULL && r2==NULL)
+            return true;
+        if(r1==NULL || r2==NULL)
+            return false;
+        
+        return (r1->val==r2->val) && ismirror(r1->left,r2->right) && ismirror(r1->right,r2->left);
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return ismirror(root,root);
+    }
+    
+    
+};
